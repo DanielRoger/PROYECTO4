3bed748dfcdfa2e4ab592b65e1a4ed038785d0f3
rogerfus <10000712.joan23@fje.edu>
2016-12-16 18:23:06 +0100

    Arreglos y mejoras del login y la interfaz gráfica

---
+++ PROYECTO_04R/bd_estoc.sql
@@ -0,0 +1,256 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 16-12-2016 a las 15:34:08
+-- Versión del servidor: 10.1.13-MariaDB
+-- Versión de PHP: 5.5.37
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bd_estoc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_categoria`
+--
+
+CREATE TABLE `tbl_categoria` (
+  `categoria_id` int(11) NOT NULL,
+  `categoria_nom` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `tbl_categoria`
+--
+
+INSERT INTO `tbl_categoria` (`categoria_id`, `categoria_nom`) VALUES
+(1, 'Dormitorio');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_comanda`
+--
+
+CREATE TABLE `tbl_comanda` (
+  `comanda_id` int(11) NOT NULL,
+  `comanda_quantitat` int(5) NOT NULL,
+  `comanda_data` date NOT NULL,
+  `prov_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_detall_com`
+--
+
+CREATE TABLE `tbl_detall_com` (
+  `detall_com_id` int(11) NOT NULL,
+  `comanda_id` int(11) NOT NULL,
+  `prod_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_estoc`
+--
+
+CREATE TABLE `tbl_estoc` (
+  `estoc_id` int(11) NOT NULL,
+  `estoc_q_max` int(5) NOT NULL,
+  `estoc_q_min` int(5) NOT NULL,
+  `prod_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `tbl_estoc`
+--
+
+INSERT INTO `tbl_estoc` (`estoc_id`, `estoc_q_max`, `estoc_q_min`, `prod_id`) VALUES
+(1, 200, 50, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_producte`
+--
+
+CREATE TABLE `tbl_producte` (
+  `prod_id` int(11) NOT NULL,
+  `prod_nom` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `pro_precio` int(6) NOT NULL,
+  `prod_foto` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `categoria_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `tbl_producte`
+--
+
+INSERT INTO `tbl_producte` (`prod_id`, `prod_nom`, `pro_precio`, `prod_foto`, `categoria_id`) VALUES
+(1, 'cama', 100, '1', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_proveidor`
+--
+
+CREATE TABLE `tbl_proveidor` (
+  `prov_id` int(11) NOT NULL,
+  `prov_nom` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
+  `prov_adre` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `prov_email` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_usuario`
+--
+
+CREATE TABLE `tbl_usuario` (
+  `usu_id` int(11) NOT NULL,
+  `usu_nombre` varchar(12) COLLATE utf8_unicode_ci NOT NULL,
+  `usu_pwd` varchar(12) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `tbl_usuario`
+--
+
+INSERT INTO `tbl_usuario` (`usu_id`, `usu_nombre`, `usu_pwd`) VALUES
+(1, 'daniel', '1234'),
+(2, 'roger', '1234'),
+(3, 'daniel', '1234'),
+(4, 'roger', '1234');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tbl_categoria`
+--
+ALTER TABLE `tbl_categoria`
+  ADD PRIMARY KEY (`categoria_id`);
+
+--
+-- Indices de la tabla `tbl_comanda`
+--
+ALTER TABLE `tbl_comanda`
+  ADD PRIMARY KEY (`comanda_id`),
+  ADD KEY `FK_comanda_prov` (`prov_id`);
+
+--
+-- Indices de la tabla `tbl_detall_com`
+--
+ALTER TABLE `tbl_detall_com`
+  ADD PRIMARY KEY (`detall_com_id`),
+  ADD KEY `FK_detall_com_prod` (`prod_id`),
+  ADD KEY `FK_detall_com_comanda` (`comanda_id`);
+
+--
+-- Indices de la tabla `tbl_estoc`
+--
+ALTER TABLE `tbl_estoc`
+  ADD PRIMARY KEY (`estoc_id`),
+  ADD KEY `FK_estoc_prod` (`prod_id`);
+
+--
+-- Indices de la tabla `tbl_producte`
+--
+ALTER TABLE `tbl_producte`
+  ADD PRIMARY KEY (`prod_id`);
+
+--
+-- Indices de la tabla `tbl_proveidor`
+--
+ALTER TABLE `tbl_proveidor`
+  ADD PRIMARY KEY (`prov_id`);
+
+--
+-- Indices de la tabla `tbl_usuario`
+--
+ALTER TABLE `tbl_usuario`
+  ADD PRIMARY KEY (`usu_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_categoria`
+--
+ALTER TABLE `tbl_categoria`
+  MODIFY `categoria_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT de la tabla `tbl_comanda`
+--
+ALTER TABLE `tbl_comanda`
+  MODIFY `comanda_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `tbl_detall_com`
+--
+ALTER TABLE `tbl_detall_com`
+  MODIFY `detall_com_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `tbl_estoc`
+--
+ALTER TABLE `tbl_estoc`
+  MODIFY `estoc_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT de la tabla `tbl_producte`
+--
+ALTER TABLE `tbl_producte`
+  MODIFY `prod_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `tbl_proveidor`
+--
+ALTER TABLE `tbl_proveidor`
+  MODIFY `prov_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `tbl_usuario`
+--
+ALTER TABLE `tbl_usuario`
+  MODIFY `usu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `tbl_comanda`
+--
+ALTER TABLE `tbl_comanda`
+  ADD CONSTRAINT `FK_comanda_prov` FOREIGN KEY (`prov_id`) REFERENCES `tbl_proveidor` (`prov_id`);
+
+--
+-- Filtros para la tabla `tbl_detall_com`
+--
+ALTER TABLE `tbl_detall_com`
+  ADD CONSTRAINT `FK_detall_com_comanda` FOREIGN KEY (`comanda_id`) REFERENCES `tbl_comanda` (`comanda_id`),
+  ADD CONSTRAINT `FK_detall_com_prod` FOREIGN KEY (`prod_id`) REFERENCES `tbl_producte` (`prod_id`);
+
+--
+-- Filtros para la tabla `tbl_estoc`
+--
+ALTER TABLE `tbl_estoc`
+  ADD CONSTRAINT `FK_estoc_prod` FOREIGN KEY (`prod_id`) REFERENCES `tbl_producte` (`prod_id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
